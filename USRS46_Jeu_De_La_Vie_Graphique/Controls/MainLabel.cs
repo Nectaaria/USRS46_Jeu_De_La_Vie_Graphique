67a4a29b022f105e04e15794d92bbd8c19aaa54a
Sarah N <sarahniederits@gmail.com>
2024-11-10 22:51:57 +0100

    Updated and fixed MainLabel

---
+++ USRS46_Jeu_De_La_Vie_Graphique/Controls/MainLabel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace USRS46_Jeu_De_La_Vie_Graphique.Controls
+{
+    public class MainLabel : Label
+    {
+        public MainLabel()
+        {
+            Name = "main_label";
+            Text = "";
+        }
+    }
+}
