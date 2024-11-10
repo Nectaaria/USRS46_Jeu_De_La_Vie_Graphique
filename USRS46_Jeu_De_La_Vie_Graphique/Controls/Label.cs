bcc1f6585fda2f457ad51ba9b1aa0b1f5d3bd108
Sarah N <sarahniederits@gmail.com>
2024-11-10 22:41:25 +0100

    Added PictureBox and Label

---
+++ USRS46_Jeu_De_La_Vie_Graphique/Controls/Label.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USRS46_Jeu_De_La_Vie_Graphique.Controls
+{
+    public class Label
+    {
+    }
+}
